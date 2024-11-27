86f7efc298652c48a68fa1c6937ab2ce2c1c9e86
Kellen Yu <kyu50@stuy.edu>
2024-11-27 10:15:00 -0500

    prelin

---
+++ prelim_calc/notes.m
@@ -0,0 +1,33 @@
+clear; clc;
+
+# What quantity do we want to minimize?
+# Time of run
+# What speed to aim for?
+# What parameters do we want?
+# - We want required torque, rpm, etc
+# - We want optimal wheel radius
+# - Within limits of power supply
+
+# What is limiting our runtime?
+# - Mass
+# - Power supply (Max current, voltage)
+# - Normal force (max acceleration)
+
+# What are limits to acceleration?
+# - At most, we accelerate for half the distance (we need to slow down too)
+# - minimize jerk?
+
+# What is a good time?
+# - sub 2
+# 10 meters under 2 seconds
+# 5 m/s minimum
+# 10 m under 1 seconds
+# 10 m/s minimum
+
+
+
+
+
+
+
+
